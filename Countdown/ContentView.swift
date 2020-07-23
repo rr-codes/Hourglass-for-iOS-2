a4f0bce59c5fb98910ddab7f9e9ead38e9e863d3
Richard Robinson <rir@my.yorku.ca>
2020-07-23 09:39:29 -0400

    Initial Commit

---
+++ Countdown/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by Richard Robinson on 2020-07-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Richard Robinson on 2020-07-23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
